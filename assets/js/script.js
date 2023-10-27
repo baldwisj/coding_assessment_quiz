48f3be1369d852fbf6604d0f1b7656a1cbcbf0bf
baldwisj <baldwisj@gmail.com>
2023-10-27 13:46:20 -0400

    Added function to append question children to question window

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,64 @@
-const startBtn = document.querySelector("#startBtn")
+const startBtn = document.getElementById("startBtn");
+const clock = document.querySelector(".clock");
 
-const question1 = {
+
+const questions = [{
     question: "Objects in JavaScript are alway enclosed by what?",
     answers: ['Square Brakets','Parentheses','Quotes', 'Curly Brakets'],
     correctAsr: 3
-};
-
-const question2 = {
-    question: "What JavaScript function allows you to use CSS selectors?",
+},
+ {
+    question: "What JavaScript method allows you to use CSS selectors?",
     answers: ['.addEventListener', '.querySelector', 'getElementbyId', '.setAttribute'],
     correctAsr: 1
-};
-
-const question3 = {
+},
+{
     question: "What is the other piece to an 'if' statement?",
     answers: ['Else', 'When', 'Otherwise', 'For'],
     correctAsr: 0
-};
-
-const question4 = {
+},
+{
     question: "How do you define a global variable?",
     answers: ['const', 'let', 'var', 'global'],
     correctAsr: 0
-};
-
-const question5 = {
+},
+{
     question: "When compaing two variables, which of the following means 'or'?",
     answers: ['&&', '!==', '||', '<='],
     correctAsr: 2
-}
+}]
+
+
 
 let timer=75
 
 function countdown(){
-    let timerInterval = setInterval(function(){
+    let timerInterval = setInterval(function() {
         timer--;
-        x.textContent = timer + "Seconds Remaining";
+        clock.textContent = timer + "Seconds Remaining";
 
         if(timer === 0) {
             clearInterval(timerinterval);
         }
     }, 1000);
 }
+
+countdown();
+
+
+const qstWindow = document.getElementById("question-window")
+
+questions.forEach(allQuestions =>{
+    let divEl = document.createElement("div");
+    let h1El = document.createElement("h1");
+    let ulEl = document.createElement("ul");
+    let liEl = document.createElement("li");
+    h1E1.textContent = allQuestions.question;
+    liEl.textContent = allQuestions.answers;
+
+    divEl.append(h1El,ulEl,liEl);
+    qstWindow.appendChild(divEl)
+});
 
 startBtn.addEventListener("click", function(event) {
     event.preventDefault();
